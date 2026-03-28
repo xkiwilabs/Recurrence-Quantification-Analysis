588fd73a34acf37eb181f1e5c64fe7f8808525b2
Mike Richardson <xkiwilabs@gmail.com>
2026-03-28 13:20:17 +1100

    Use pybind11 setup helpers for cross-platform builds

--- setup.py
+++ setup.py
@@ -3,25 +3,18 @@
 All package metadata is in pyproject.toml. This file exists only because
 setuptools cannot declare C++ extensions in pyproject.toml yet.
 """
-from setuptools import setup, Extension
-import pybind11
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup
 import os
-import sys
-
-extra_compile_args = []
-if sys.platform == "win32":
-    extra_compile_args = ["/std:c++14"]
-else:
-    extra_compile_args = ["-std=c++14"]
 
 ext_modules = [
-    Extension(
+    Pybind11Extension(
         "rqa_analysis.utils.rqa_utils_cpp",
         [os.path.join("rqa_analysis", "utils", "rqa_utils.cpp")],
-        include_dirs=[pybind11.get_include()],
-        language="c++",
-        extra_compile_args=extra_compile_args,
     )
 ]
 
-setup(ext_modules=ext_modules)
+setup(
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+)
